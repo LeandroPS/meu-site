0704dd9c2bcd7d63d4be5392b5fd422537c55cef
Leandro Pires de Souza <leandro.pires.souza@gmail.com>
2017-09-11 11:24:56 -0300

    alterações no Tetris

---
+++ tetris/js/basic_questions.js
@@ -0,0 +1,422 @@
+var basic_questions = [
+  {
+    statement:"Tonight I am going <div class='line'></div> the movies.",
+    options:[
+      {
+        correct: false,
+        text: "for"
+      },
+      {
+        correct: true,
+        text: "to"
+      },
+      {
+        correct: false,
+        text: "at"
+      },
+      {
+        correct: false,
+        text: "by"
+      }
+    ]
+  },
+  {
+    statement:"I study in the <div class='line'></div>.",
+    options:[
+      {
+        correct: false,
+        text: "10 o'clock"
+      },
+      {
+        correct: true,
+        text: "morning"
+      },
+      {
+        correct: false,
+        text: "night"
+      },
+      {
+        correct: false,
+        text: "tuesday"
+      }
+    ]
+  },
+  {
+    statement:"Peter <div class='line'></div> 18 years old.",
+    options:[
+      {
+        correct: false,
+        text: "am"
+      },
+      {
+        correct: false,
+        text: "have"
+      },
+      {
+        correct: false,
+        text: "has"
+      },
+      {
+        correct: true,
+        text: "is"
+      }
+    ]
+  },
+  {
+    statement:"Some day I will travel to <div class='line'></div>.",
+    options:[
+      {
+        correct: false,
+        text: "the France."
+      },
+      {
+        correct: false,
+        text: "a France."
+      },
+      {
+        correct: false,
+        text: "for France."
+      },
+      {
+        correct: true,
+        text: "France."
+      }
+    ]
+  },
+  {
+    statement:"Despite <div class='line'></div> age, he is very attractive.",
+    options:[
+      {
+        correct: false,
+        text: "your"
+      },
+      {
+        correct: true,
+        text: "his"
+      },
+      {
+        correct: false,
+        text: "her"
+      },
+      {
+        correct: false,
+        text: "its"
+      }
+    ]
+  },
+  {
+    statement:"Laura has three <div class='line'></div>.",
+    options:[
+      {
+        correct: false,
+        text: "childs"
+      },
+      {
+        correct: false,
+        text: "child"
+      },
+      {
+        correct: false,
+        text: "childres"
+      },
+      {
+        correct: true,
+        text: "children"
+      }
+    ]
+  },
+  {
+    statement:"Where would you go to buy bread?",
+    options:[
+      {
+        correct: true,
+        text: "the bakery"
+      },
+      {
+        correct: false,
+        text: "the barber shop"
+      },
+      {
+        correct: false,
+        text: "the gym"
+      },
+      {
+        correct: false,
+        text: "the pub"
+      }
+      
+    ]
+  },
+  {
+    statement:"There is a good show <div class='line'></div>television tonight.",
+    options:[
+      {
+        correct: false,
+        text: "in"
+      },
+      {
+        correct: false,
+        text: "of"
+      },
+      {
+        correct: false,
+        text: "at"
+      },
+      {
+        correct: true,
+        text: "on"
+      }
+    ]
+  },
+  {
+    statement:"The policemen raised <div class='line'></div>guns.",
+    options:[
+      {
+        correct: false,
+        text: "They’re"
+      },
+      {
+        correct: false,
+        text: "They are"
+      },
+      {
+        correct: false,
+        text: "There"
+      },
+      {
+        correct: true,
+        text: "Their"
+      }
+    ]
+  },
+  {
+    statement:"British economy is going to face <div class='line'></div> year of recession.",
+    options:[
+      {
+        correct: false,
+        text: "the other"
+      },
+      {
+        correct: false,
+        text: "the other"
+      },
+      {
+        correct: false,
+        text: "an other"
+      },
+      {
+        correct: true,
+        text: "another"
+      }
+    ]
+  },
+  {
+    statement:"<div class='line'></div> you like horror movies?",
+    options:[
+      {
+        correct: false,
+        text: "Have"
+      },
+      {
+        correct: false,
+        text: "Does"
+      },
+      {
+        correct: false,
+        text: "Is"
+      },
+      {
+        correct: true,
+        text: "Do"
+      }
+    ]
+  },
+  {
+    statement:"Are you <div class='line'></div> to music now?",
+    options:[
+      {
+        correct: false,
+        text: "listen"
+      },
+      {
+        correct: false,
+        text: "listens"
+      },
+      {
+        correct: true,
+        text: "listening"
+      },
+      {
+        correct: false,
+        text: "listened"
+      }
+    ]
+  },
+  {
+    statement:"Jerry and I <div class='line'></div> going to the beach.",
+    options:[
+      {
+        correct: true,
+        text: "are"
+      },
+      {
+        correct: false,
+        text: "is"
+      },
+      {
+        correct: false,
+        text: "am"
+      },
+      {
+        correct: false,
+        text: "ain't"
+      }
+    ]
+  },
+    {
+    statement:"I <div class='line'></div> study hard before a test.",
+    options:[
+      {
+        correct: false,
+        text: "to"
+      },
+      {
+        correct: true,
+        text: "always"
+      },
+      {
+        correct: false,
+        text: "have"
+      },
+      {
+        correct: false,
+        text: "go"
+      }
+    ]
+  },
+  {
+    statement:"My brother <div class='line'></div> a good book collection.",
+    options:[
+      {
+        correct: true,
+        text: "has"
+      },
+      {
+        correct: false,
+        text: "have"
+      },
+      {
+        correct: false,
+        text: "haves"
+      },
+      {
+        correct: false,
+        text: "don't have"
+      }
+    ]
+  },
+  {
+    statement:"Yesterday, we <div class='line'></div> to the club.",
+    options:[
+      {
+        correct: false,
+        text: "goed"
+      },
+      {
+        correct: false,
+        text: "will go"
+      },
+      {
+        correct: false,
+        text: "want"
+      },
+      {
+        correct: true,
+        text: "went"
+      }
+    ]
+  },
+  {
+    statement:"The patients are <div class='line'></div> to heal.",
+    options:[
+      {
+        correct: true,
+        text: "beginning"
+      },
+      {
+        correct: false,
+        text: "begining"
+      },
+      {
+        correct: false,
+        text: "began"
+      },
+      {
+        correct: false,
+        text: "begins"
+      }
+    ]
+  },
+  {
+    statement:"My birthday is <div class='line'></div> December 5th.",
+    options:[
+      {
+        correct: false,
+        text: "in"
+      },
+      {
+        correct: true,
+        text: "on"
+      },
+      {
+        correct: false,
+        text: "the"
+      },
+      {
+        correct: false,
+        text: "at"
+      }
+    ]
+  },
+  {
+    statement:"those are <div class='line'></div> cars.",
+    options:[
+      {
+        correct: true,
+        text: "his"
+      },
+      {
+        correct: false,
+        text: "yours"
+      },
+      {
+        correct: false,
+        text: "mine"
+      },
+      {
+        correct: false,
+        text: "ours"
+      }
+    ]
+  },
+  {
+    statement:"My boyfriend <div class='line'></div> live in London.",
+    options:[
+      {
+        correct: false,
+        text: "not"
+      },
+      {
+        correct: false,
+        text: "don't"
+      },
+      {
+        correct: false,
+        text: "no"
+      },
+      {
+        correct: true,
+        text: "doesn't"
+      }
+    ]
+  }];
--- tetris/js/script.js
+++ tetris/js/script.js
@@ -1,6 +1,19 @@
 var points = 0;
 var all = 0;
 
+function getDuration(level){
+  var stackHeight = $("div.content."+level+" div.inner div.question.stuck").size()*75;
+  var frameHeight =  (new_height = Math.floor($(".content.play").outerHeight()/75)*75) + 50;
+  
+  var height = frameHeight - stackHeight;
+  
+  var duration = Math.ceil((5000*height)/frameHeight);
+  console.log("duration: "+duration);
+
+  return duration;
+  
+}
+
 function next(){
   var index = $("article.show").index() +1;
   $("article.show").removeClass("show").addClass("gone");
@@ -8,22 +21,64 @@ function next(){
   $("article:nth-child("+(index+1)+")").addClass("show");
 }
 
-function nextAnimation(){
+function nextBasicAnimation(){
   all++;
-  var height = $("div.inner div.question.stuck").size()*75;
+  var height = $("div.content.basic div.inner div.question.stuck").size()*75;
   if(height+75<$(".content.play").outerHeight()){
-    $("div.inner div.question:not(.stuck):not(.correct)").first().addClass("current").animate({bottom: height}, 5000, "linear", function(){
+    $("div.content.basic div.inner div.question:not(.stuck):not(.correct)").first().addClass("current").animate({bottom: height}, getDuration("basic"), "linear", function(){
       $(this).addClass("stuck");
-      nextAnimation();
+      nextBasicAnimation();
     });
   }else{
     setTimeout(function(){
       $("article.show").removeClass("show").addClass("gone");
-      $("article:nth-child(4)").addClass("show");
+      $("article:nth-child(8)").removeClass("gone").addClass("show");
     }, 2500);
   }
   
-  if($("div.inner div.question:not(.stuck):not(.correct)").size()==0){
+  if($("div.content.basic div.inner div.question:not(.stuck):not(.correct)").size()==0){
+    setTimeout(next, 2500);
+    
+  }
+}
+
+function nextMediumAnimation(){
+  all++;
+  var height = $("div.content.medium div.inner div.question.stuck").size()*75;
+  if(height+75<$(".content.play").outerHeight()){
+    $("div.content.medium div.inner div.question:not(.stuck):not(.correct)").first().addClass("current").animate({bottom: height}, 5000, "linear", function(){
+      $(this).addClass("stuck");
+      nextMediumAnimation();
+    });
+  }else{
+    setTimeout(function(){
+      $("article.show").removeClass("show").addClass("gone");
+      $("article:nth-child(8)").removeClass("gone").addClass("show");
+    }, 2500);
+  }
+  
+  if($("div.content.medium div.inner div.question:not(.stuck):not(.correct)").size()==0){
+    setTimeout(next, 2500);
+    
+  }
+}
+
+function nextAdvancedAnimation(){
+  all++;
+  var height = $("div.content.advanced div.inner div.question.stuck").size()*75;
+  if(height+75<$(".content.play").outerHeight()){
+    $("div.content.advanced div.inner div.question:not(.stuck):not(.correct)").first().addClass("current").animate({bottom: height}, 5000, "linear", function(){
+      $(this).addClass("stuck");
+      nextAdvancedAnimation();
+    });
+  }else{
+    setTimeout(function(){
+      $("article.show").removeClass("show").addClass("gone");
+      $("article:nth-child(8)").removeClass("gone").addClass("show");
+    }, 2500);
+  }
+  
+  if($("div.content.advanced div.inner div.question:not(.stuck):not(.correct)").size()==0){
     setTimeout(next, 2500);
     
   }
@@ -39,26 +94,80 @@ $(function(){
   //new_height = Math.floor($(".content.play").outerHeight()/$("div.inner div.question").first().outerHeight());
   new_height = Math.floor($(".content.play").outerHeight()/75)*75;
   
+  var levels = [basic_questions, medium_questions, advanced_questions];
+  
+  var levelNames = ["basic", "medium", "advanced"];
+  
+  $.each(levels, function( levelIndex, level) {
+    $.each(level, function( index, question ) {
+      var cont = jQuery("<div></div>").addClass("question");
+      var statement = jQuery("<span></span>").html(question.statement).addClass("statement").appendTo(cont);
+
+      var ul = jQuery("<ul></ul>").addClass("answers");
+      $.each(question.options, function( index, option ) {
+        var li = jQuery("<li></li>").addClass(option.correct? "correct": "wrong").text(option.text).appendTo(ul);
+      });
+
+      ul.appendTo(cont);
+
+      //$("div.content.basic div.inner")
+      cont.appendTo("div.content."+levelNames[levelIndex]+" div.inner");
+    });
+    
+  });
+  
+  
+  
+  
   console.log($("span.advice").outerHeight());
   $(".content.play").height(new_height + 50);
   
   
   $("div.inner div.question").css("bottom", $(".content.play").outerHeight());
-  $("button.start").click(function(){
+  $("button.start.basic").click(function(){
     next();
-    nextAnimation();
+    nextBasicAnimation();
   });
   
-  $("div.question:not(.stuck) ul li.correct").click(function(){
+  $("div.inner div.question").css("bottom", $(".content.play").outerHeight());
+  $("button.start.medium").click(function(){
+    next();
+    nextMediumAnimation();
+  });
+  
+  $("div.inner div.question").css("bottom", $(".content.play").outerHeight());
+  $("button.start.advances").click(function(){
+    next();
+    nextAdvancedAnimation();
+  });
+  
+  $("div.content.basic div.inner div.question:not(.stuck) ul li.correct").click(function(){
     if(!$(this).parents(".question").hasClass("stuck")){
       increasePoints();
       $(this).parents("div.question").stop(true, false).addClass("correct");
-      nextAnimation();
+      nextBasicAnimation();
+    }
+  });
+  
+  $("div.content.medium  div.inner div.question:not(.stuck) ul li.correct").click(function(){
+    if(!$(this).parents(".question").hasClass("stuck")){
+      increasePoints();
+      $(this).parents("div.question").stop(true, false).addClass("correct");
+      nextMediumAnimation();
+    }
+  });
+  
+  $("div.content.advanced div.inner div.question:not(.stuck) ul li.correct").click(function(){
+    if(!$(this).parents(".question").hasClass("stuck")){
+      increasePoints();
+      $(this).parents("div.question").stop(true, false).addClass("correct");
+      nextAdvancedAnimation();
     }
   });
   
   $("div.question:not(.stuck) ul li:not(.correct)").click(function(){
     if(!$(this).parents(".question").hasClass("stuck")){
+      $(this).parents(".question").addClass("stuck");
       $(this).addClass("clicked");
     }
   });
