b57e5275283177c548cae77a00b17f5179679303
Leandro Pires de Souza <leandro.pires.souza@gmail.com>
2017-09-18 17:06:26 -0300

    readicionando tetris

---
+++ tetris/js/advanced_questions.js
@@ -0,0 +1,401 @@
+var advanced_questions = [
+  {
+    statement:"Which word is a synonym for “acquire”?",
+    options:[
+      {
+        correct: false,
+        text: "demonstrate"
+      },
+      {
+        correct: false,
+        text: "testify"
+      },
+      {
+        correct: true,
+        text: "obtain"
+      }
+    ]
+  },
+  
+  
+  {
+    statement:"If I <div class='line'></div> about his problem, I would have tried to help.",
+    options:[
+      {
+        correct: false,
+        text: "have knew"
+      },
+      
+      {
+        correct: true,
+        text: "had known"
+      },
+      {
+        correct: false,
+        text: "knew"
+      }
+    ]
+  },
+  {
+    statement:"By the time George arrives, we will <div class='line'></div> the meeting.",
+    options:[
+      {
+        correct: false,
+        text: "finish"
+      },
+      {
+        correct: false,
+        text: "have finished"
+      },
+      {
+        correct: false,
+        text: "finish"
+      }
+    ]
+  },
+  {
+    statement:"Charles is not <div class='line'></div>enough to believe in pyramid schemes.",
+    options:[
+      {
+        correct: true,
+        text: "gullible"
+      },
+      {
+        correct: false,
+        text: "clever"
+      },
+      {
+        correct: false,
+        text: "keen"
+      }
+    ]
+  },
+  {
+    statement:"New technologies gradually tend to <div class='line'></div> the older ones obsolete.",
+    options:[
+      {
+        correct: false,
+        text: "reach"
+      },
+      {
+        correct: true,
+        text: "rend"
+      },
+      {
+        correct: false,
+        text: "remain"
+      }
+    ]
+  },
+  {
+    statement:"His statement, despite being true, was <div class='line'></div> misleading.",
+    options:[
+      {
+        correct: false,
+        text: "thus"
+      },
+      {
+        correct: false,
+        text: "moreover"
+      },
+      {
+        correct: true,
+        text: "nevertheless"
+      }
+    ]
+  },
+  {
+    statement:"Which of these words is a synonym for “jeopardize”?",
+    options:[
+      {
+        correct: true,
+        text: "threaten"
+      },
+      {
+        correct: false,
+        text: "cherish"
+      },
+      {
+        correct: false,
+        text: "encourage"
+      }
+    ]
+  },
+  {
+    statement:"If I were you, I <div class='line'></div> hit that button.",
+    options:[
+      {
+        correct: true,
+        text: "wouldn't"
+      },
+      {
+        correct: false,
+        text: "didn't"
+      },
+      {
+        correct: false,
+        text: "cannot"
+      }
+    ]
+  },
+  {
+    statement:"I've <div class='line'></div> since I was a kid.",
+    options:[
+      {
+        correct: false,
+        text: "meet"
+      },
+      {
+        correct: false,
+        text: "knew"
+      },
+      {
+        correct: true,
+        text: "known"
+      }
+    ]
+  },
+  {
+    statement:"He was charged <div class='line'></div> a murder.",
+    options:[
+      {
+        correct: true,
+        text: "with"
+      },
+      {
+        correct: false,
+        text: "of"
+      },
+      {
+        correct: true,
+        text: "on"
+      }
+    ]
+  },
+  {
+    statement:"Megan, <div class='line'></div> son is a musician, can also sing very well.",
+    options:[
+      {
+        correct: false,
+        text: "that"
+      },
+      {
+        correct: false,
+        text: "a"
+      },
+      {
+        correct: true,
+        text: "whose"
+      }
+    ]
+  },
+  {
+    statement:"The mayor <div class='line'></div> to reduce poverty levels this year.",
+    options:[
+      {
+        correct: false,
+        text: "oath"
+      },
+      {
+        correct: false,
+        text: "delt"
+      },
+      {
+        correct: true,
+        text: "undertook"
+      }
+    ]
+  },
+  {
+    statement:"The senator's argument doesn't hold <div class='line'></div>.",
+    options:[
+      {
+        correct: false,
+        text: "valid"
+      },
+      {
+        correct: true,
+        text: "water"
+      },
+      {
+        correct: false,
+        text: "credit"
+      }
+    ]
+  },
+  {
+    statement:"Wich word is not a synonym for 'dead'?",
+    options:[
+      {
+        correct: false,
+        text: "deceased"
+      },
+      {
+        correct: true,
+        text: "fading"
+      },
+      {
+        correct: false,
+        text: "expired"
+      }
+    ]
+  },
+  
+  {
+    statement:"Ann has set her <div class='line'></div> on getting a promotion.",
+    options:[
+      {
+        correct: false,
+        text: "head"
+      },
+      {
+        correct: false,
+        text: "brain"
+      },
+      {
+        correct: true,
+        text: "heart"
+      }
+    ]
+  },
+  {
+    statement:"To get our way, we often have to <div class='line'></div> the rules a little.",
+    options:[
+      {
+        correct: true,
+        text: "bend"
+      },
+      {
+        correct: false,
+        text: "split"
+      },
+      {
+        correct: false,
+        text: "move"
+      }
+    ]
+  },
+  {
+    statement:"I finished the job in ten minutes. I was easy as <div class='line'></div>.",
+    options:[
+      {
+        correct: true,
+        text: "pie"
+      },
+      {
+        correct: false,
+        text: "milk"
+      },
+      {
+        correct: false,
+        text: "candy"
+      }
+    ]
+  },
+  {
+    statement:"Which word is a synonym for “bid”?",
+    options:[
+      {
+        correct: false,
+        text: "evaluation"
+      },
+      {
+        correct: false,
+        text: "demand"
+      },
+      {
+        correct: true,
+        text: "offer"
+      }
+    ]
+  },
+  {
+    statement:"Make sure to <div class='line'></div> these opportunities while you can.",
+    options:[
+      {
+        correct: false,
+        text: "touch"
+      },
+      {
+        correct: false,
+        text: "halt"
+      },
+      {
+        correct: true,
+        text: "seize"
+      }
+    ]
+  },
+  
+  {
+    statement:"The sergeant couldn’t <div class='line'></div> the fear of his soldiers.",
+    options:[
+      {
+        correct: false,
+        text: "persuade"
+      },
+      {
+        correct: true,
+        text: "quell"
+      },
+      {
+        correct: false,
+        text: "move"
+      }
+    ]
+  },
+  
+  
+  
+  {
+    statement:"Severe measures <div class='line'></div> to be taken to avoid undesired consequences.",
+    options:[
+      {
+        correct: true,
+        text: "ought"
+      },
+      {
+        correct: false,
+        text: "are"
+      },
+      {
+        correct: false,
+        text: "has"
+      }
+    ]
+  },
+  {
+    statement:"If I <div class='line'></div> about his problem, I would have tried to help.",
+    options:[
+      {
+        correct: false,
+        text: "have knew"
+      },
+      {
+        correct: true,
+        text: "had known"
+      },
+      {
+        correct: false,
+        text: "knew"
+      }
+    ]
+  },
+  {
+    statement:"By the time George arrives, we will <div class='line'></div> the meeting.",
+    options:[
+      {
+        correct: true,
+        text: "have finished"
+      },
+      {
+        correct: false,
+        text: "go to finish"
+      },
+      {
+        correct: false,
+        text: "finish"
+      }
+    ]
+  }
+];
---
+++ tetris/js/basic_questions.js
@@ -0,0 +1,343 @@
+var basic_questions = [
+  {
+    statement:"Tonight I am going <div class='line'></div> the movies.",
+    options:[
+      {
+        correct: false,
+        text: "for"
+      },
+      {
+        correct: true,
+        text: "to"
+      },
+      {
+        correct: false,
+        text: "by"
+      }
+    ]
+  },
+  {
+    statement:"I study in the <div class='line'></div>.",
+    options:[
+      {
+        correct: false,
+        text: "10 o'clock"
+      },
+      {
+        correct: true,
+        text: "morning"
+      },
+      {
+        correct: false,
+        text: "tuesday"
+      }
+    ]
+  },
+  {
+    statement:"Peter <div class='line'></div> 18 years old.",
+    options:[
+      {
+        correct: false,
+        text: "have"
+      },
+      {
+        correct: false,
+        text: "has"
+      },
+      {
+        correct: true,
+        text: "is"
+      }
+    ]
+  },
+  {
+    statement:"Some day I will travel to <div class='line'></div>.",
+    options:[
+      {
+        correct: false,
+        text: "the France."
+      },
+      {
+        correct: false,
+        text: "a France."
+      },
+      {
+        correct: true,
+        text: "France."
+      }
+    ]
+  },
+  {
+    statement:"Despite <div class='line'></div> age, he is very attractive.",
+    options:[
+      {
+        correct: false,
+        text: "your"
+      },
+      {
+        correct: true,
+        text: "his"
+      },
+      {
+        correct: false,
+        text: "its"
+      }
+    ]
+  },
+  {
+    statement:"Laura has three <div class='line'></div>.",
+    options:[
+      {
+        correct: false,
+        text: "childs"
+      },
+      {
+        correct: false,
+        text: "child"
+      },
+      {
+        correct: true,
+        text: "children"
+      }
+    ]
+  },
+  {
+    statement:"Where would you go to buy bread?",
+    options:[
+      {
+        correct: true,
+        text: "the bakery"
+      },
+      {
+        correct: false,
+        text: "the gym"
+      },
+      {
+        correct: false,
+        text: "the pub"
+      }
+      
+    ]
+  },
+  {
+    statement:"There is a good show <div class='line'></div>television tonight.",
+    options:[
+      {
+        correct: false,
+        text: "in"
+      },
+      {
+        correct: false,
+        text: "at"
+      },
+      {
+        correct: true,
+        text: "on"
+      }
+    ]
+  },
+  {
+    statement:"The policemen raised <div class='line'></div>guns.",
+    options:[
+      {
+        correct: false,
+        text: "They’re"
+      },
+      {
+        correct: false,
+        text: "There"
+      },
+      {
+        correct: true,
+        text: "Their"
+      }
+    ]
+  },
+  {
+    statement:"British economy is going to face <div class='line'></div> year of recession.",
+    options:[
+      {
+        correct: false,
+        text: "the other"
+      },
+      {
+        correct: false,
+        text: "an other"
+      },
+      {
+        correct: true,
+        text: "another"
+      }
+    ]
+  },
+  {
+    statement:"<div class='line'></div> you like horror movies?",
+    options:[
+      {
+        correct: false,
+        text: "Have"
+      },
+      {
+        correct: false,
+        text: "Does"
+      },
+      {
+        correct: true,
+        text: "Do"
+      }
+    ]
+  },
+  {
+    statement:"Are you <div class='line'></div> to music now?",
+    options:[
+      {
+        correct: false,
+        text: "listen"
+      },
+      {
+        correct: true,
+        text: "listening"
+      },
+      {
+        correct: false,
+        text: "listens"
+      }
+      
+    ]
+  },
+  {
+    statement:"Jerry and I <div class='line'></div> going to the beach.",
+    options:[
+      {
+        correct: true,
+        text: "are"
+      },
+      {
+        correct: false,
+        text: "is"
+      },
+      {
+        correct: false,
+        text: "am"
+      }
+    ]
+  },
+    {
+    statement:"I <div class='line'></div> study hard before a test.",
+    options:[
+      {
+        correct: false,
+        text: "to"
+      },
+      {
+        correct: true,
+        text: "always"
+      },
+      {
+        correct: false,
+        text: "have"
+      }
+    ]
+  },
+  {
+    statement:"My brother <div class='line'></div> a good book collection.",
+    options:[
+      {
+        correct: true,
+        text: "has"
+      },
+      {
+        correct: false,
+        text: "have"
+      },
+      {
+        correct: false,
+        text: "don't have"
+      }
+    ]
+  },
+  {
+    statement:"Yesterday, we <div class='line'></div> to the club.",
+    options:[
+      {
+        correct: false,
+        text: "goed"
+      },
+      {
+        correct: false,
+        text: "will go"
+      },
+      {
+        correct: true,
+        text: "went"
+      }
+    ]
+  },
+  {
+    statement:"The patients are <div class='line'></div> to heal.",
+    options:[
+      {
+        correct: true,
+        text: "beginning"
+      },
+      {
+        correct: false,
+        text: "began"
+      },
+      {
+        correct: false,
+        text: "begins"
+      }
+    ]
+  },
+  {
+    statement:"My birthday is <div class='line'></div> December 5th.",
+    options:[
+      {
+        correct: false,
+        text: "in"
+      },
+      {
+        correct: true,
+        text: "on"
+      },
+      {
+        correct: false,
+        text: "at"
+      }
+    ]
+  },
+  {
+    statement:"those are <div class='line'></div> cars.",
+    options:[
+      {
+        correct: true,
+        text: "his"
+      },
+      {
+        correct: false,
+        text: "yours"
+      },
+      {
+        correct: false,
+        text: "ours"
+      }
+    ]
+  },
+  {
+    statement:"My boyfriend <div class='line'></div> live in London.",
+    options:[
+      {
+        correct: false,
+        text: "not"
+      },
+      {
+        correct: false,
+        text: "don't"
+      },
+      {
+        correct: true,
+        text: "doesn't"
+      }
+    ]
+  }];
